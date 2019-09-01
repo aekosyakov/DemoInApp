a2e0bf4ca7a3098b2a9ea3d2d65018bf6addd7a3
Macbook Air <macbookair@MacBook-Air-Macbook.local>
2019-09-01 04:09:36 +0300

    clean

--- DemoScreen/AppDelegate.swift
+++ DemoScreen/AppDelegate.swift
@@ -18,8 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
-        window!.rootViewController = demoModule.viewController
+        window!.rootViewController = UINavigationController(rootViewController: demoModule.viewController) 
         window!.makeKeyAndVisible()
+        
+        demoModule.handleAction = { action in
+            print(action!)
+        }
         return true
     }
 
--- DemoScreen/Modules/DemoModule.swift
+++ DemoScreen/Modules/DemoModule.swift
@@ -6,34 +6,23 @@
 //
 
 import UIKit
-import Reachability
 import Firebase
 
 final
 class DemoModule {
     
     var handleAction:((DemoAction?) -> Void)?
-
-    fileprivate lazy
-    var iapService = IAPService().with {
-        $0.loadAvailableSKProducts()
-        $0.completion = { [weak self] status in self?.transactionCompletion(status) }
-    }
-
-    private
-    let reachability = Reachability()
-    
-    private lazy
-    var tableViewController = TableViewController().with {
-        $0.didPress = { [weak self] variant in self?.showPurchaseScreen(variant: variant) }
-    }
     
     var viewController: UIViewController {
         return tableViewController
     }
+    
+    private lazy
+    var tableViewController = TableViewController().with {
+        $0.didPress = { [weak self] variant in self?.showDemoScreen(variant: variant) }
+    }
 
     init() {
-        try? reachability?.startNotifier()
         FirebaseApp.configure()
         let remoteConfig = RemoteConfig.remoteConfig()
         remoteConfig.fetch(withExpirationDuration: TimeInterval(0)) { status, error in
@@ -46,65 +35,17 @@ class DemoModule {
             self.tableViewController.variants = Array(keys)
         }
     }
-
-    private
-    func transactionCompletion(_ status: PurchaseStatus) {
-        switch status {
-        case .success:
-            showConfetti()
-        case .failed(let error):
-            displayErrorIfNeeded(error)
-        }
-    }
     
-    public
-    func showPurchaseScreen(variant: String) {
+    private
+    func showDemoScreen(variant: String) {
         let jsonDict = RemoteConfig.remoteConfig().configValue(forKey: variant).jsonValue as? [String: Any]
         guard let uiConfig = UIConfig(jsonDict: jsonDict) else {
             return
         }
-        uiConfig.isPremiumPurchased = self.iapService.isPremiumPurchased
         
         let demoViewController = DemoViewController(uiConfig: uiConfig)
         demoViewController.action = self.handleAction
         self.viewController.present(demoViewController, animated: true, completion: nil)
-    }
-
-}
-
-private
-extension DemoModule {
-
-    func displayErrorIfNeeded(_ error: Error?) {
-        guard let error = error else {
-            return
-        }
-        viewController.showAlert(title: error.localizedDescription)
-    }
-
-    func showConfetti() {
-        viewController.view.displayEmitterCells()
-    }
-
-}
-
-private
-let cantMakePaymentsAlertTitle = "Sorry, this device is not able or allowed to make payments"
-
-private
-let internetConnectionAlertTitle = "Please check your Internet connection"
-
-
-private
-extension Reachability.Connection {
-
-    var isValid: Bool {
-        switch self {
-        case .cellular, .wifi:
-            return true
-        default:
-            return false
-        }
     }
 
 }
